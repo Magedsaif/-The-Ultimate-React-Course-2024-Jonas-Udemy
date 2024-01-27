670fd037006120dee91048833a278c79d60afe0c
Maged Saif <magedsaif21@gmail.com>
2024-01-27 17:27:58 +0200

    creating a state variable with setState

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,21 +1,29 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
+// use state is a hook
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
@@ -31,7 +39,7 @@ export default function App() {
         </button>
         <button
           style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-          onClick={() => alert("Next")}
+          onClick={handleNext}
         >
           Next
         </button>

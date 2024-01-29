4bda2cd1bf9c9a51815aa761878450a6ae5b8d6b
Maged Saif <magedsaif21@gmail.com>
2024-01-29 15:13:52 +0200

    controlled elements

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,21 +21,40 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip ?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
       <button>Add</button>
     </form>
   );
@@ -68,3 +89,5 @@ function Stats() {
     </footer>
   );
 }
+
+// controlled elements in React are elements that are controlled by React and not by the DOM itself (input, textarea, select) and we control them by using the value attribute and the onChange event handler, and we use the state to store the value of the input and we update the state with the onChange event handler and we use the value attribute to set the value of the input to the value of the state so we can control the input from React and not from the DOM itself.

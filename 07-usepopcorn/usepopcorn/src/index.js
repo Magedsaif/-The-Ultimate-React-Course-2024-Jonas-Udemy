ab6309f57adf4e475846375c5da8fd73ccc2544e
magedsaif <magedsaif21@gmail.com>
2024-02-07 18:54:36 +0200

    add

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -10,7 +10,5 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5}></StarRating>
-    <StarRating maxRating={10}></StarRating>
-    <StarRating></StarRating>
   </React.StrictMode>
 );

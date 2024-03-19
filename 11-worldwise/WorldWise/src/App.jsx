470b0498c6fb370d8e5aaf4f78b34aa3c544cd41
Maged Saif <magedsaif21@gmail.com>
2024-03-19 22:18:05 +0200

    buliding tha app layout

--- 11-worldwise/WorldWise/src/App.jsx
+++ 11-worldwise/WorldWise/src/App.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="product" element={<Product></Product>}></Route>
-        <Route path="pricing" element={<Pricing></Pricing>}></Route>
-        <Route path="app" element={<AppLayout></AppLayout>}></Route>
-        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );

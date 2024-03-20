dc941145ecb6eb0f4b4db3ea4555b578b157db83
magedsaif <magedsaif21@gmail.com>
2024-03-20 11:09:09 +0200

    implementing the cities list

--- 11-worldwise/WorldWise/src/App.jsx
+++ 11-worldwise/WorldWise/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -6,8 +6,29 @@ import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error Loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,9 +36,33 @@ export default function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <CityList cities={cities} isLoading={isLoading}></CityList>
+            }
+          ></Route>
+
+          <Route
+            path="cities"
+            element={
+              <CityList cities={cities} isLoading={isLoading}></CityList>
+            }
+          ></Route>
+
+          <Route path="countries" element={<p>Countries</p>}></Route>
+
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+// we need nested Routes when we want a part of the UI to be controlled by a part of the URL.
+
+// the nested route is considered to be one when it has an influence on what component is rendered inside the bigger component
+
+// the index route is the default child route that is going to be matched if none of other routes in the parent Route matches
--- 11-worldwise/WorldWise/src/components/Sidebar.jsx
+++ 11-worldwise/WorldWise/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 
 export default function SideBar() {
   return (
     <div className={styles.sidebar}>
       <Logo></Logo>
       <AppNav></AppNav>
-      <p>List of cities</p>
+      <Outlet></Outlet>
 
       <footer className={styles.footer}>
         <p className={styles.copyright}>

94d641d086b1e7901fb62c03b02e4d27c6fdfbcb
Maged Saif <magedsaif21@gmail.com>
2024-03-29 23:58:43 +0200

    Reading and Setting a Query String

--- 11-worldwise/WorldWise/src/components/City.jsx
+++ 11-worldwise/WorldWise/src/components/City.jsx
@@ -1,8 +1,11 @@
 import styles from "./City.module.css";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
@@ -15,7 +18,12 @@ function City() {
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <h1>City {id}</h1>
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lng}, {lat}
+      </p>
+    </>
 
     //   <div className={styles.city}>
     //     <div className={styles.row}>

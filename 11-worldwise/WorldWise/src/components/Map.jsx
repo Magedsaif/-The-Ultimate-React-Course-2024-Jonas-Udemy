cf3481d2eeca9ab6fb41152507729e986866ba15
magedsaif <magedsaif21@gmail.com>
2024-04-15 13:05:21 +0200

    fetching city data in  the form

--- 11-worldwise/WorldWise/src/components/Map.jsx
+++ 11-worldwise/WorldWise/src/components/Map.jsx
@@ -12,17 +12,16 @@ import {
 import { useEffect, useState } from "react";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   //programatic navigation
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const { cities } = useCities();
-  const [searchParams] = useSearchParams();
-
+  
   const {isLoading: isLoadingPosition, position: geolocationPosition, getPosition} = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  
+const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {

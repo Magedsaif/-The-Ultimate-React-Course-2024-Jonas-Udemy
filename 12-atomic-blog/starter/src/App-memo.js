2d00b537efe7337f8cdec618fc0279d936b96624
Magedsaf <Magedsaif21@gmail.com>
2024-05-02 14:06:40 +0300

    edit

--- 12-atomic-blog/starter/src/App-memo.js
+++ 12-atomic-blog/starter/src/App-memo.js
@@ -33,7 +33,7 @@ function App() {
     setPosts([]);
   }
 
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool) yes.
   useEffect(
     function () {
       document.documentElement.classList.toggle("fake-dark-mode");

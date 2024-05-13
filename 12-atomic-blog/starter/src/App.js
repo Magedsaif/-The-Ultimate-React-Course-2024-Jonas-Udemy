10601555c08f28105ea8434f52db59662275811c
magedsaif <magedsaif21@gmail.com>
2024-05-13 09:17:48 +0200

    edit

--- 12-atomic-blog/starter/src/App.js
+++ 12-atomic-blog/starter/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
+import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -118,19 +119,22 @@ function FormAddPost() {
       <button>Add post</button>
     </form>
   );
-}
+} 
 
 function List() {
   const { posts } = usePosts();
   return (
+    <>
     <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <Test></Test>
+    </>
   );
 }
 

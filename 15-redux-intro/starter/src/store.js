85f72d7fd4e62f4d0bd132414a3ec4ce18118bf1
magedsaif <magedsaif21@gmail.com>
2024-05-13 10:24:46 +0200

    edit

--- 15-redux-intro/starter/src/store.js
+++ 15-redux-intro/starter/src/store.js
@@ -3,7 +3,7 @@ import { createStore } from "redux";
 const initialState = {
   balance: 0,
   loan: 0,
-  LoanPurpose: "",
+  loanPurpose: "",
 };
 
 function reducer(state = initialState, action) {
@@ -14,12 +14,17 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
         loan: 0,
-        LoanPurpose: "",
+        loanPurpose: "",
         balance: state.balance - state.loan,
       };
 
@@ -31,3 +36,13 @@ function reducer(state = initialState, action) {
 const store = createStore(reducer);
 
 store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());

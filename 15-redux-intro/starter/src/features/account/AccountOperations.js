10cb73feda5fc3c406dbbb8fa670f38bacbcb08f
Magedsaf <Magedsaif21@gmail.com>
2024-07-23 19:03:51 +0300

    edit

--- 15-redux-intro/starter/src/features/account/AccountOperations.js
+++ 15-redux-intro/starter/src/features/account/AccountOperations.js
@@ -14,13 +14,16 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
   console.log(balance);
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount));
+
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -59,8 +62,6 @@ function AccountOperations() {
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
           </select>
-
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
         </div>
 
         <div>
--- 15-redux-intro/starter/src/store.js
+++ 15-redux-intro/starter/src/store.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk"; // Correctly import the named export 'thunk'
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-//we should compine all the reducers we have
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
